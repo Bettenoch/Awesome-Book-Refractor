7e1142479fce1a9c5e208bc73e089d7b80c12ca0
Bett Enock <bettenock968@gmail.com>
2023-07-17 22:58:41 -0700

    add .j  and change selector

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import bookItems from './modules/booklist';
-import CreateBook from './modules/newBookItem';
-import fetchFromLstorage from './modules/fetchLocalStorage';
-import { DeleteBook, deleteFromStorage } from './modules/deleteBook';
-import displayBooks from './modules/displayBooks';
-import { DateTime } from './modules/luxon';
+import bookItems from './modules/booklist.js';
+import CreateBook from './modules/newBookItem.js';
+import fetchFromLstorage from './modules/fetchLocalStorage.js';
+import { DeleteBook, deleteFromStorage } from './modules/deleteBook.js';
+import displayBooks from './modules/displayBooks.js';
+import { DateTime } from './modules/luxon.js';
 
 const date = DateTime.fromObject({ }, { zone: 'system' }).toLocaleString(DateTime.DATE_FULL);
 const time = DateTime.fromObject({ }, { zone: 'system' }).toLocaleString(DateTime.TIME_SIMPLE);
@@ -12,9 +12,9 @@ const addBookItem = document.querySelector('.form-container');
 const bookList = document.getElementById('bookList');
 const bookListContainer = document.getElementById('book-list-content');
 const contactDoc = document.getElementById('contact-holder');
-const MainLink = document.getElementById('Main-Link');
-const AddPageLink = document.getElementById('Add-Page-Link');
-const contactPageLink = document.getElementById('Contact_Link');
+const MainLink = document.querySelectorAll('.Main-Link');
+const AddPageLink = document.querySelectorAll('.Add-Page-Link');
+const contactPageLink = document.querySelectorAll('.Contact_Link');
 const dateSection = document.querySelector('.date-time');
 
 dateSection.innerHTML = `${date} ${time}`;
@@ -49,14 +49,14 @@ formField.addEventListener('submit', (event) => {
     addBookItem.style.display = 'flex';
   } else {
     const book = new bookItems(title, author, Date.now());
-    bookItems.push(book);
+    books.push(book);
     CreateBook.addBook(book);
     fetchFromLstorage();
 
-    localStorage.setItem('bookInfo', JSON.stringify(bookItems));
+    localStorage.setItem('bookInfo', JSON.stringify(books));
 
-    title.value = '';
-    author.value = '';
+    titleInput.value = '';
+    authorInput.value = '';
   }
 });
 
