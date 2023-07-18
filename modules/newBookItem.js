c81d42f7d81b9441787732633eae650cc92d2d98
Bett Enock <bettenock968@gmail.com>
2023-07-17 22:56:55 -0700

    change classname and add styling

--- modules/newBookItem.js
+++ modules/newBookItem.js
@@ -3,15 +3,15 @@ const bookList = document.getElementById('bookList');
 export default class CreateBook {
     static addBook(book) {
         const bookItem = document.createElement('li');
-        bookItem.className = 'books-list';
+        bookItem.className = 'books-list-item';
         bookItem.id = book.id;
         bookItem.innerHTML = `
             <p class="book-title">${book.title}</p> 
             <p>By</p>
             <p class="book-author">${book.author}</p>
             <button class='removeBook'>Remove</button>
-        `
+        `;
         bookList.appendChild(bookItem);
-        
+        bookList.style.border = '3px solid black';
     }
 }
