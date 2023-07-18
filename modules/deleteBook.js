c5edb754e2e759972bf334cf44c51ec4056b0e86
Bett Enock <bettenock968@gmail.com>
2023-07-17 19:25:27 -0700

    Deleting selected book Items

---
+++ modules/deleteBook.js
@@ -0,0 +1,28 @@
+import fetchFromLstorage from "./fetchLocalStorage";
+
+const bookList = document.getElementById('bookList');
+const bookListContainer = document.getElementById('book-list-content');
+
+export class DeleteBook {
+    static deleteBookFromPage(target) {
+        if (target.classList.contains('removeBook')) {
+          target.parentElement.remove();
+        }
+    
+        if (!bookList.firstElementChild) {
+          booksDiv.style.border = '3px solid red';
+        }
+      }
+
+      static displayOnLoad() {
+        bookListContainer.style.display = 'flex';
+      } 
+}
+
+export const deleteFromStorage = (item) => {
+    const books = fetchFromLstorage();
+    const { id } = item.parentElement;
+    const index = books.findIndex((book) => book.id === id);
+    books.splice(index, 1);
+    localStorage.setItem('bookInfo', JSON.stringify(books));
+};
